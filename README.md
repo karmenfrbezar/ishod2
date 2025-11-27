6418743dac7ab564fb69e1da5d0ca97c37bd5cf9
Karmen Frbezar <karmen.frbezar7@gmail.com>
2025-11-27 12:58:53 +0100

    Zadaci do 7.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ishod2
 
 # Provjera 2. ishoda, Karmen Frbežar, 27.11.2025.
+
+# http://localhost:9000/#/unos
---
+++ frontend/quasar-project/src/pages/UnosPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <h2>Unos page</h2>
+    <q-btn color="white" text-color="black" label="Gumb 1" />
+    <q-btn color="primary" label="Gumb 2" />
+  </div>
+</template>
